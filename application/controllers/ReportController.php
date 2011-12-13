f523f45c0f7d704adfbf4bbc68bb5a6a8096a538
liukai <liukai@wildfire.asia>
2011-12-13 17:33:43 +0800

    fix report download

--- application/controllers/ReportController.php
+++ application/controllers/ReportController.php
@@ -1877,7 +1877,7 @@ function adminreportbatchreplyAction(){
 						$selectAccessCode = $db->select();
 						$selectAccessCode->from('report', array('consumer_id','accesscode', 'create_date', 'source'))
 						->joinLeft('reward_point_transaction_record', 'report.reward_point_transaction_record_id = reward_point_transaction_record.id', 'point_amount')
-						->join('consumer', 'consumer.id = report.consumer_id', array('email,login_phone,recipients_name'))
+						->join('consumer', 'consumer.id = report.consumer_id', array('email','login_phone','recipients_name'))
 						->where('report.campaign_id = ?',$formData['campaign_id'])
 						->where("report.state = 'APPROVED'")
 						->limit(0);
