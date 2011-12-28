899df30eb9c98584a0bce6e2371763e5b0ef0601
BruceLiu <liuhuazeng@xingxinghuo.com>
2011-12-28 17:10:44 +0800

    update friend

--- application/controllers/CampaignController.php
+++ application/controllers/CampaignController.php
@@ -523,7 +523,7 @@ class CampaignController extends MyController
 		}
 		//whether participate in the campaign 2011-05-19 ham.bao
 		$campaigninvitationModel = new CampaignInvitation();
-		$campaigninvitation = $campaigninvitationModel->fetchRow('campaign_id = '.$id.' and consumer_id'.' ='.$consumer->id);
+		$campaigninvitation = $campaigninvitationModel->fetchRow('campaign_id = '.$id.' and consumer_id'.' ='.$consumer->id);	
 		if($campaigninvitation == null){
 			$this->_helper->redirector('index','home');
 		}
@@ -677,16 +677,16 @@ class CampaignController extends MyController
 			$campaignInvitation->save();
              
 			//2011-05-19 ham.bao add the badge
-			$consumerBadgeModel = new ConsumerBadge();
-			$consumerBadgeData  = $consumerBadgeModel->fetchRow('badge='.$this->view->campaign->badge .' and consumer='.$consumer->id);
-			if(!count($consumerBadgeData)){
-				$row = $consumerBadgeModel->createRow();
-				$row->consumer = $consumer->id;
-				$row->badge    = $this->view->campaign->badge;
-				$row->create_date = date("Y-m-d H:i:s");
-				$row->save();
-
-			}
+//			$consumerBadgeModel = new ConsumerBadge();
+//			$consumerBadgeData  = $consumerBadgeModel->fetchRow('badge='.$this->view->campaign->badge .' and consumer='.$consumer->id);
+//			if(!count($consumerBadgeData)){
+//				$row = $consumerBadgeModel->createRow();
+//				$row->consumer = $consumer->id;
+//				$row->badge    = $this->view->campaign->badge;
+//				$row->create_date = date("Y-m-d H:i:s");
+//				$row->save();
+//
+//			}
 			//2011-05-19 ham.bao add the badge
 			
 			$result = $db->fetchOne(
@@ -774,7 +774,7 @@ class CampaignController extends MyController
 		}
 
 		$this->view->form = $form;
-		$this->view->relative = $this->view->campaign->relative;
+		$this->view->friendsLimit = $this->view->campaign->relative;
 	}
 
 	function postcampaignAction(){
