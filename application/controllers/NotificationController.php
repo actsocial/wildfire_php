4127f56be3700630299b4ea20694e1b90efa3e21
BruceLiu <liuhuazeng@xingxinghuo.com>
2012-04-27 12:11:08 +0800

    add notification

---
+++ application/controllers/NotificationController.php
@@ -0,0 +1,18 @@
+<?php
+class NotificationController extends MyController{
+	function deleteAction(){
+		
+	}
+	function addAction(){
+	}
+	
+	function showAction(){
+		$select = $db->select();
+		$this->view->notificates="";
+	}
+	
+	function modifyAction(){
+	
+	}
+}
+
