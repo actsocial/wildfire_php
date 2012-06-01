dc949fb44fad21af44d2a8d3a52185d37c791883
liuzhehui <liuzhehui@xingxinghuo.com>
2012-06-01 16:21:21 +0800

    fix

--- library/weibo/weiboconfig.php
+++ library/weibo/weiboconfig.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
 
-define( "WB_AKEY" , '3931627059' );
-define( "WB_SKEY" , '6940d6ccdccce9feb53e78cc8aeaf056' );
+define( "WB_AKEY" , '1769492638' );
+define( "WB_SKEY" , 'bad24d573f9ca068108f19fdb7a777a8' );
 define( "WB_CALLBACK_URL" , 'http://local.xingxinghuo.cn/public/weibologin/callback' );
