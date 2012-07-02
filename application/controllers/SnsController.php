4a5f3a0c1a7270efcda0cc388844b00b35804468
Bruce Zhang <zhangchencheng@xingxinghuo.com>
2012-07-02 18:16:02 +0800

    sns

--- application/controllers/SnsController.php
+++ application/controllers/SnsController.php
@@ -12,13 +12,11 @@ class SnsController extends MyController
 	  $sns_users = $db->fetchAll($select);
 		$this->view->users = $sns_users;
 		
-		$selet = $db->select()
-								->from('sns_user');
-	  $all_users =$db->fetchAll($select);
-		$this->view->all_users = $all_users;
-		
+		$sns = new Sns();
+		$this->view->sns_enabled_source = $sns->get_enable_source();
+
 		$config = Zend_Registry::get('config');
-		//$this->view->writer_host = $config->writer
+		$this->view->writer_host = $config->writer->host;
 
 	}
   
--- application/models/Sns.php
+++ application/models/Sns.php
@@ -1,17 +1,14 @@
 <?php
 class Sns
 {
-	public function initialize(type,token,refresh_token,secret,expires_in,expires_at) {
-    $type = type;
-    $token = token;
-    $refresh_token = refresh_token;
-    $secret = secret;
-    $expires_in = expires_in.to_i;
-    $expires_at = expires_at.to_i;
-    if(oauth_info.is){
-    	
-    }
-      
-  }
-	
+	public function get_enable_source() {
+		return array(
+			'Weibo' => array('name' => 'Weibo'),
+			'Douban'=> array('name' => 'Douban'),
+			'Renren' => array('name' => 'Renren'),
+			'Tencent' => array('name' => 'Tencent'),
+			'Kaixin' => array('name' => 'Kaixin'),
+			'Netease' => array('name' => 'Netease'),
+		);
+	}
 }
