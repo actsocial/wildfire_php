a964e4ea6464e62f5c1679b7c7fb9011cd23c4f4
Bruce Zhang <zhangchencheng@xingxinghuo.com>
2012-07-02 14:13:07 +0800

    sns

---
+++ application/models/Sns.php
@@ -0,0 +1,17 @@
+<?php
+class Sns
+{
+	public function initialize(type,token,refresh_token,secret,expires_in,expires_at) {
+    $type = type;
+    $token = token;
+    $refresh_token = refresh_token;
+    $secret = secret;
+    $expires_in = expires_in.to_i;
+    $expires_at = expires_at.to_i;
+    if(oauth_info.is){
+    	
+    }
+      
+  }
+	
+}
