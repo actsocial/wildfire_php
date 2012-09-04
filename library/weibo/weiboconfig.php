4b16d57a1a7059c3f1258f08549dfc1fbd74cdc5
liuhuazeng <liuhuazeng@xingxinghuo.com>
2012-09-04 11:04:33 +0800

    open weibo login

--- library/weibo/weiboconfig.php
+++ library/weibo/weiboconfig.php
@@ -3,4 +3,4 @@ header('Content-Type: text/html; charset=UTF-8');
 
 define( "WB_AKEY" , '1864117054' );
 define( "WB_SKEY" , 'a533d04ae435c90b14c5286e74d37bb8' );
-define( "WB_CALLBACK_URL" , 'http://home.xingxinghuo.com/wildfire_php/public/weibologin/callback' );
+define( "WB_CALLBACK_URL" , 'http://home.xingxinghuo.com/public/weibologin/callback' );
