1b3f88e050a3a204c475d15237b8df59d9466c78
liuzhehui <liuzhehui@xingxinghuo.com>
2012-11-27 14:59:56 +0800

    add ssl

--- library/facebook-php-sdk/FacebookLoginAuthAdaptor.php
+++ library/facebook-php-sdk/FacebookLoginAuthAdaptor.php
@@ -12,12 +12,18 @@ class FacebookLoginAuthAdaptor implements Zend_Auth_Adapter_Interface {
 	
 	public function authenticate() {
 		$db = Zend_Registry::get ( 'db' );
-
-		$consumer = $db->fetchOne("SELECT *  FROM consumer WHERE email=:email", array('email'=>$this->_facebookemail));
+		$consumer = $db->fetchOne("SELECT *  FROM consumer WHERE facebookid=:facebookid", array('facebookid'=>$this->_facebookid));
 		if($consumer) {
 			return new Zend_Auth_Result(Zend_Auth_Result :: SUCCESS, $this->_facebookid);
 		}else {
-			return new Zend_Auth_Result(Zend_Auth_Result :: FAILURE, $this->_facebookid);
+			$consumer = $db->fetchOne("SELECT *  FROM consumer WHERE email=:email", array('email'=>$this->_facebookemail));
+			if($consumer) {
+				$consumer->facebookid = $this->_facebookid;
+				$consumer.save();
+				return new Zend_Auth_Result(Zend_Auth_Result :: SUCCESS, $this->_facebookid);
+			}else {
+				return new Zend_Auth_Result(Zend_Auth_Result :: FAILURE, $this->_facebookid);
+			}
 		}
 	}
 }
