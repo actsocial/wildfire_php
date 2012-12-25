90685e0f8dafc40789cc6d84ba2eaa8edb611759
liuhuazeng <liuhuazeng@xingxinghuo.com>
2012-12-25 18:12:50 +0800

    fix the facebook login and register page and so on

--- library/facebook-php-sdk/facebookconfig.php
+++ library/facebook-php-sdk/facebookconfig.php
@@ -7,5 +7,5 @@ define( "WB_CALLBACK_URL" , 'http://home.xingxinghuo.com/public/weibologin/callb
 
 define( "FB_AKEY" , '234132663359597' );
 define( "FB_SKEY" , '1886a959cd90f54b377613f8140a5669' );
-define( "FB_CALLBACK_URL" , 'http://community.wildfire.asia/public/facebooklogin/index' );
-define( "FB_REGISTER_CALLBACK_URL" , 'http://community.wildfire.asia/public/facebooklogin/register' );
+define( "FB_CALLBACK_URL" , 'http://ph.wildfire.asia/public/facebooklogin/index' );
+define( "FB_REGISTER_CALLBACK_URL" , 'http://ph.wildfire.asia/public/facebooklogin/register' );
